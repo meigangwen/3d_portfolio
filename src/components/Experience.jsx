229fd6e306a105ce85ec776de994b58603f3c31c
fancilivain <meigangwen@gmail.com>
2024-02-22 11:32:27 +0800

    part1 finished

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Environment, OrbitControls, Sky, ContactShadows } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 
@@ -16,6 +16,14 @@ export const Experience = () => {
       <Sky />
       <Environment preset="sunset" />
       <group position-y={-1}>
+        <ContactShadows 
+          opacity={0.42} 
+          scale={10} 
+          blur={1} 
+          far={10} 
+          resolution={256} 
+          color="#000000" 
+        />
         <Avatar animation={animation} />
         { animation === "Typing" && (
           <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25} >
@@ -23,7 +31,7 @@ export const Experience = () => {
             <meshStandardMaterial color="white" />
           </mesh>
         )}
-        <mesh scale={5} rotation-x={-Math.PI * 0.5}>
+        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
           <planeGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
